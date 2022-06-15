a6b8215df04e4eaf1ebce6ecfce1a9a3f311bd8e
Shalin Hasanbhai Awadiya <sh290595@dal.ca>
2022-06-15 20:31:55 -0300

    Post complain UI done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import './App.scss';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import 'react-toastify/dist/ReactToastify.css';
-import ShowProducts from './pages/products/ShowProducts';
+import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import ShowProducts from "./pages/products/ShowProducts";
 
-import Home from './pages/home/Home';
-import SignUp from './pages/signup';
-import Header from './components/header';
-import Login from './pages/login';
-import ForgetPassword from './pages/forget-password';
-import Footer from './components/footer';
-import CouponsHomePage from './pages/coupon/CouponsHomePage';
-import SeachPage from './pages/search-page';
-import ComplainForm from './pages/compain/ComplainForm';
-import ComplainTable from './pages/compain/ComplainTable';
-import ReplyTable from './pages/compain/ReplyTable';
-import Cart from './pages/cart/Cart';
-import Checkout from './pages/checkout/checkout';
-import Profile from './pages/profile';
+import Home from "./pages/home/Home";
+import SignUp from "./pages/signup";
+import Header from "./components/header";
+import Login from "./pages/login";
+import ForgetPassword from "./pages/forget-password";
+import Footer from "./components/footer";
+import CouponsHomePage from "./pages/coupon/CouponsHomePage";
+import SeachPage from "./pages/search-page";
+import ComplainForm from "./pages/compain/ComplainForm";
+import ComplainTable from "./pages/compain/ComplainTable";
+import ReplyTable from "./pages/compain/ReplyTable";
+import Cart from "./pages/cart/Cart";
+import Checkout from "./pages/checkout/checkout";
+import Profile from "./pages/profile";
 import ProductPage from "./pages/products/ProductPage";
 import ShowReviews from "./pages/reviews/ShowReviews";
 
@@ -31,14 +31,14 @@ function App() {
             <Route path="/signup" element={<SignUp />} />
             <Route path="/login" element={<Login />} />
             <Route path="/coupons" element={<CouponsHomePage />} />
-            <Route path='/forget-password' element={<ForgetPassword />} />
-            <Route path='/search' element={<SeachPage />} />
-            <Route path='/complains' element={<ComplainForm />} />
-            <Route path='/view_complains' element={<ComplainTable />} />
-            <Route path='/replied_complain' element={<ReplyTable />} />
-            <Route path='/checkout' element={<Checkout />} />
-            <Route path='/cart' element={<Cart />}></Route>
-            <Route path='/show_products' element={<ShowProducts />}></Route>
+            <Route path="/forget-password" element={<ForgetPassword />} />
+            <Route path="/search" element={<SeachPage />} />
+            <Route path="/post_complain" element={<ComplainForm />} />
+            <Route path="/view_complains" element={<ComplainTable />} />
+            <Route path="/replied_complain" element={<ReplyTable />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/cart" element={<Cart />}></Route>
+            <Route path="/show_products" element={<ShowProducts />}></Route>
             <Route path="/product" element={<ProductPage />} />
             <Route path="/product/review" element={<ShowReviews />} />
             <Route path="/profile" element={<Profile />}></Route>
--- src/pages/compain/ComplainForm.js
+++ src/pages/compain/ComplainForm.js
@@ -1,95 +1,138 @@
-import React from "react";
-import "./ComplainForm.css";
+import * as React from "react";
+import { useState } from "react";
+import { TextField, Button, Box } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
-const ComplainForm = () => {
+export default function ComplainForm() {
+  const navigate = useNavigate();
+
+  const [complainSubject, setComplainSubject] = useState("");
+  const [complainDescription, setComplainDescription] = useState("");
+  const [complainAttachment, setComplainAttachment] = useState("");
+
+  const [complainSubjectError, setComplainSubjectError] = useState("");
+  const [complainDescriptionError, setComplainDescriptionError] = useState("");
+
+  const complainSubjectHandler = (event) => {
+    setComplainSubject(event.target.value);
+
+    if (event.target.value === "") {
+      setComplainSubjectError("Complain Subject cannot be empty");
+    } else {
+      setComplainSubjectError("");
+    }
+  };
+  const complainDescriptionHandler = (event) => {
+    setComplainDescription(event.target.value);
+
+    if (event.target.value === "") {
+      setComplainDescriptionError("Complain Description cannot be empty");
+    } else {
+      setComplainDescriptionError("");
+    }
+  };
+  const complainAttachmentHandler = (event) => {
+    setComplainAttachment(event.target.value);
+  };
+
+  const submitButtonHandler = (event) => {
+    event.preventDefault();
+    let complainSubjectCheck = true;
+    let complainDescriptionCheck = true;
+    console.log(complainSubject, complainDescription, complainAttachment);
+    if (complainSubject === "") {
+      complainSubjectCheck = false;
+      setComplainSubjectError("Complain Subject cannot be empty");
+    }
+    if (complainDescription === "") {
+      complainDescriptionCheck = false;
+      setComplainDescriptionError("Complain Description cannot be empty");
+    }
+    if (complainSubjectCheck === true && complainDescriptionCheck === true) {
+      navigate("/");
+    }
+  };
+
+  const viewComplainHandler = (event) => {
+    navigate("/viewComplain");
+  };
+
   return (
-    <div className="form-center mt-5 mx-auto rounded w-75">
-      <h2 className="text-center">Post Complain</h2>
-      <form action="/viewComplain">
-        <table>
-          <tr>
-            <td>
-              <label className="fonts" for="subject">
-                Complain Subject:
-              </label>
-              <br />
-              <br />
-            </td>
-            <td>
-              <input
-                className="form-control mt-3"
-                type="text"
-                id="fname"
-                name="fname"
-                style={{ "font-style": "italic" }}
-                placeholder="Enter  Complain  Subject"
-              />
-              <br />
-              <br />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <label className="fonts" for="textarea">
-                Complain Description:
-              </label>
-              <br />
-              <br />
-            </td>
-            <td>
-              <textarea
-                name="textarea"
-                id="textarea"
-                className="form-control"
-                style={{ "font-style": "italic" }}
-                required
-                placeholder="Enter Complain Description"
-              ></textarea>
-              <br />
-              <br />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <label className="fonts" for="file">
-                Complain Attachment(if):
-              </label>
-              <br />
-              <br />
-              <br />
-              <br />
-            </td>
-            <td>
-              <input type="file" className="form-control mt-3" id="file" name="file" />
-              <br />
-              <br />
-              <br />
-              <br />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <button
-                type="submit"
-                className="btn btn-primary"
-                style={{
-                  "background-color": "rgb(95, 148, 241)",
-                  color: "white",
-                }}
-                value="Submit"
-              >
-                Submit
-              </button>
-              &nbsp;
-              <button type="cancel" className="btn btn-secondary" value="Cancel">
-                Cancel
-              </button>
-            </td>
-          </tr>
-        </table>
+    <Box
+      border={2}
+      borderColor="rgb(26,125,230)"
+      height={650}
+      width={500}
+      padding="20px"
+      margin="auto"
+      marginTop="100px"
+      display="flex"
+    >
+      <form>
+        <h2>Post Complain</h2>
+        <TextField
+          required
+          multiline
+          id="name-input"
+          name="name"
+          label="Complain Subject"
+          type="text"
+          style={{ width: "350px" }}
+          onChange={complainSubjectHandler}
+        />
+        <br />
+        <span style={{ color: "red", fontSize: "10px" }}>
+          {complainSubjectError}
+        </span>
+        <br />
+        <br />
+        <TextField
+          required
+          multiline
+          rows={4}
+          id="name-input"
+          name="name"
+          label="Complain Description"
+          type="text"
+          style={{ width: "350px" }}
+          onChange={complainDescriptionHandler}
+        />
+        <br />
+        <span style={{ color: "red", fontSize: "10px" }}>
+          {complainDescriptionError}
+        </span>
+        <br />
+        <br />
+        <label>
+          Complain Attachment(if any)
+          <br />
+          <br />
+          <input
+            type="file"
+            id="file"
+            name="file"
+            onChange={complainAttachmentHandler}
+          />
+        </label>
+        <br />
+        <br />
+        <br />
+        <Button onClick={submitButtonHandler} variant="contained">
+          Submit
+        </Button>
+        &nbsp;
+        <Button variant="contained">Cancel</Button>
+        <br />
+        <br />
+        <br />
+        <Button
+          style={{ width: "200px" }}
+          onClick={viewComplainHandler}
+          variant="outlined"
+        >
+          View Complains
+        </Button>
       </form>
-    </div>
+    </Box>
   );
-};
-
-export default ComplainForm;
+}
