b7b3e4b21f462636e9f15da5d11e9aed5de730aa
Shalin Hasanbhai Awadiya <sh290595@dal.ca>
2022-07-14 17:14:59 -0300

    Added model for Complain Management

---
+++ server/models/complain.js
@@ -0,0 +1,57 @@
+//Shalin Hasanbhai Awadiya - B00892907
+const mongoose = require("mongoose");
+
+const complainSchema = new mongoose.Schema({
+  complainId: {
+    type: String,
+    required: false,
+  },
+  complainSubject: {
+    type: String,
+    required: false,
+  },
+  complainDescription: {
+    type: String,
+    required: false,
+  },
+  complainDate: {
+    type: String,
+    required: false,
+  },
+  complainTime: {
+    type: String,
+    required: false,
+  },
+  complainStatus: {
+    type: String,
+    required: false,
+  },
+  complainImage: {
+    type: String,
+    required: false,
+  },
+  complainFrom_LoginId: {
+    type: Number,
+    required: false,
+  },
+  replySubject: {
+    type: String,
+    required: false,
+  },
+  replyMessage: {
+    type: String,
+    required: false,
+  },
+  replyDate: {
+    type: String,
+    required: false,
+  },
+  replyTime: {
+    type: String,
+    required: false,
+  },
+});
+
+const ComplainModel = mongoose.model("Complain", complainSchema);
+
+module.exports = ComplainModel;
